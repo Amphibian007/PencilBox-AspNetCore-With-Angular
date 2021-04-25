9a106d815691632e36afd79a8f9219104d7091c8
Dipto Das <diptod111@gmail.com>
2021-04-25 12:27:28 +0600

    Added project 1 code

---
+++ source/StartingProject/Project1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LargerBetweenFirstAndLastArrayElement(new int[] { 6, 3, 45, 3 }));
+        }
+        public static int LargerBetweenFirstAndLastArrayElement(int[] myArray)
+        {
+            if (myArray[0] > myArray[myArray.Length - 1])
+                return myArray[0];
+            else
+                return myArray[myArray.Length - 1];
+        }
+    }
+}
